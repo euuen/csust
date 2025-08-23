558247d47825e3560c6c87f239ab6abc4e5f6390
euuen <3048721251@qq.com>
2025-08-23 17:39:58 +0800

    giscustalk

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,10 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import { h, toRefs } from 'vue'
+import type { Theme} from 'vitepress'
+import { useData, useRoute } from 'vitepress';
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'
 import test from "./test.vue"
 
 export default {
@@ -13,7 +15,24 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
     app.component('test', test)
+  },
+  setup() {
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+    giscusTalk({
+            repo: 'euuen/csust',
+            repoId: 'R_kgDOPiwftw',
+            category: 'General', // 或其他分类
+            categoryId: 'DIC_kwDOPiwft84Cuf_M',
+            mapping: 'pathname',
+            inputPosition: 'top',
+            lang: 'zh-CN',
+            loading: 'lazy',
+            theme: "preferred_color_scheme"
+        }, {
+            frontmatter,
+            route
+        }, true);
   }
 } satisfies Theme
